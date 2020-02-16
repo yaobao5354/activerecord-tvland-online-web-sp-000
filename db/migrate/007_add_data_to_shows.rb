49d47b4b0b814ad745de84fef8c785be3deb7c50
yaoyu bao <yaobao5354@gmail.com>
2020-02-16 22:31:31 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_data_to_shows.rb
+++ db/migrate/007_add_data_to_shows.rb
@@ -1,6 +1,8 @@
 class AddDataToShows < ActiveRecord::Migration[5.1]
 
   def change
-    add_column :shows, :day, :genre, :season, :string
+    add_column :shows, :day, :string
+    add_column :shows, :genre, :string
+    add_column :shows, :season, :string
   end
 end
