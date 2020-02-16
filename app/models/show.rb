ef7cffaf65d1f090bbb78a8b616deb270106ece3
yaoyu bao <yaobao5354@gmail.com>
2020-02-16 22:31:49 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    actors_array = []
+    self.actors.each do |actor|
+      actors_array << actor.full_name
+    end
+    actors_array
+  end
 end
