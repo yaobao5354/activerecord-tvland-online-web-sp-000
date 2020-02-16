93bd5c4fc17b28bbe360c4d31a58bbf98cbb7afb
yaoyu bao <yaobao5354@gmail.com>
2020-02-16 21:37:00 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,7 +1,6 @@
 class CreateActors < ActiveRecord::Migration[5.1]
   def change
     create_table :actors do |t|
-      #primary key of :id is created for us!
       t.string :first_name
       t.string :last_name
     end
